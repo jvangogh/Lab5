6efc43b9520f8c61d3015330ecce70c54596ada6
Joshua van Gogh <jvangogh@ucsd.edu>
2015-02-12 16:05:48 -0800

    Add an author/email comment

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello
     public static void main( String argv[] )
     {
 	// Default is "World"
+	// Author: Joshua van Gogh (jvangogh@somewhere.com)
         String name = "World";
         if ( argv.length != 0 )
         {
